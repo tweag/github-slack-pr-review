84a0c773e9bc53e082eb570ba77704b4fbe1c7ce
Ray Zane <ray@promptworks.com>
2016-01-20 17:03:03 -0500

    Messages were a bit too overbearing

--- lib/slack_notifier.rb
+++ lib/slack_notifier.rb
@@ -26,36 +26,10 @@ class SlackNotifier
       icon_url: IMAGES.sample,
       username: 'Github PR Assignment',
       attachments: [{
-        fallback: "#{name}, please review this PR: #{link}",
-        pretext: "#{name}, please review this PR:",
-        text: pull_request['body'],
+        fallback: "#{name}, review #{link}",
+        pretext: name,
         title: pull_request['title'],
-        title_link: link,
-        fields: [{
-          title: 'Assignee',
-          value: name,
-          short: true
-        }, {
-          title: 'Author',
-          value: pull_request['user']['login'],
-          short: true
-        }, {
-          title: 'Additions',
-          value: pull_request['additions'],
-          short: true
-        }, {
-          title: 'Deletions',
-          value: pull_request['deletions'],
-          short: true
-        }, {
-          title: 'Commits',
-          value: pull_request['commits'],
-          short: true
-        }, {
-          title: 'Changed Files',
-          value: pull_request['changed_files'],
-          short: true
-        }]
+        title_link: link
       }]
     }
   end
